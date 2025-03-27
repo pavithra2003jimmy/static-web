a1d2fc53fd1cd45cbc29ed1154cc58883a82cb17
pavithra <98955198+pavithra2003jimmy@users.noreply.github.com>
2025-03-27 13:43:07 +0530

    Delete task1.js

--- task1.js
+++
@@ -1,8 +0,0 @@
-let num = [2,5,6,7,8,9,12,14,15,16,18,0]
-let even=[];
-for(let i=0;i<num.length;i++)
-{
-    if(num[i]%2==0)
-        even.push(num[i]);
-}
-console.log( "THE NUMBERS THAT ARE EVEN ARE: ", even);
